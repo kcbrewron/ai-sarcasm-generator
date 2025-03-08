f47d53688a8471990f670d9eb573363e19f891b7
Ron Nelson <ronnelson929@gmail.com>
2025-03-08 09:05:15 -0800

    Adding all the methods to like and manage the sarcasm

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
+import packageJSON from '../package.json'
 import { Hono } from "hono";
 import { apiReference } from '@scalar/hono-api-reference';
+import openAPISpecs from 'hono-openapi'
 import sarcasm from './routes/sarcasm';
+
+
 // Start a Hono app
 const app = new Hono();
 
 app.route("/", sarcasm);
-// Setup OpenAPI registry
-const openapi = apiReference(app, {
-	docs_url: "/",
-});
 
 
+app.get(
+	'/openapi',
+	openAPISpecs(app, {
+		documentation: {
+			info: {
+				title: 'Sarcasm Generator and rating API',
+				version: `${packageJSON.version}`,
+				description: 'Sarcasm API',
+			},
+			servers: [
+				{ url: 'http://localhost:8787', description: 'Local Server' },
+			],
+		},
+	})
+)
+
+
+app.get(
+	'/docs',
+	apiReference({
+		theme: 'saturn',
+		spec: { url: '/openapi' },
+	})
+)
 
 // Export the Hono app
 export default app;
--- src/routes/sarcasm.js
+++ src/routes/sarcasm.js
@@ -1,11 +1,61 @@
 import { Hono } from 'hono';
 import { getSarcasticComments, addOneSarcasticComment, likeSarcasticComment } from '../dao/sarcasmDAO';
+import { describeRoute } from 'hono-openapi';
+
 
 const sarcasm = new Hono();
 /**
  * Get all 
  */
-sarcasm.get("/", async (c)=>{
+sarcasm.get("/",describeRoute({
+    description: 'Get all sarcast comments',
+    summary: "Get Sarcastic Comments",
+    tags: ["Sarcasm"],
+    responses: {
+        200: {
+            description: "Successful Resposne",
+            'application/json': {
+                schema: {
+                    type: "object",
+                    properties: {
+                        id: {
+                            description: 'The unique identifier of the sarcastic comment',
+                            example: crypto.randomUUID(),
+                            type: 'string',
+                        },
+                        prompt: {
+                            description: 'The prompt entered for sarcastic comment',
+                            example: "When I test, I test in production",
+                            type: 'string',
+                        },
+                        category: {
+                            description: 'AI Generated category of the sarcastic comment',
+                            example: "Testing",
+                            type: 'string',
+                        },
+                        sarcastic_comment: {
+                            description: 'The comment of the sarcastic comment',
+                            example: "long string from ai",
+                            type: 'string',
+                        },
+                        likes: {
+                            description: 'The number of likes for the sarcastic comment',
+                            example: crypto.randomUUID(),
+                            type: 'number',
+                        },
+                        created_ts: {
+                            description: 'The date when it was created sarcastic comment',
+                            example: `${new Date().toISOString}`,
+                            type: 'string',
+                            format: 'datetime',
+                        }
+
+                    }
+                }
+            }
+        }
+    },
+}), async (c)=>{
     const results = await getSarcasticComments(c.env)
     const processedResults = results;
 
@@ -19,7 +69,7 @@ sarcasm.get("/", async (c)=>{
  */
 sarcasm.patch("/:id",async(c)=>{
     const id = await c.req.id;
-    const results = await likeSarcasticComment(id);
+    const results = await likeSarcasticComment(id,c.env);
     return results;
 })
 
@@ -27,7 +77,10 @@ sarcasm.patch("/:id",async(c)=>{
  * Create one
  */
 sarcasm.post("/",async(c)=>{
+    const body = await c.req.json();
+    console.log(`Sarcastic comment created ${JSON.stringify(body)}`);
 
+    const results = addOneSarcasticComment(body,c.env)
 })
 
 export default sarcasm;
